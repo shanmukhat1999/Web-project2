2df71cfd024039f6ed692c90588cdcd9a36c2c74
shanmukhat1999 <57249299+shanmukhat1999@users.noreply.github.com>
2021-01-15 14:05:08 +0530

    Update application.py

--- application.py
+++ application.py
@@ -103,9 +103,8 @@ def new_message(data):
     emit('add message',{"message":k,"chatid":i},broadcast=True)     
 
  
-
-if __name__ == "__main__":
-    app.run(debug=True)       
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)     
 
 
 
