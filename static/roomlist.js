5f2dfc65d957e4938b189b8ab3faf0092c058cad
shanmukhat1999 <57249299+shanmukhat1999@users.noreply.github.com>
2021-01-21 10:51:10 +0530

    Update roomlist.js

--- static/roomlist.js
+++ static/roomlist.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Cannot submit empty value for input field
     document.querySelector('button').disabled = true;
     document.querySelector('input').onkeyup = () => {
         if(document.querySelector('input').value.length>0)
@@ -13,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     
-
+    //reload when back/front are used in the browser
     if ((document.querySelector('#refresh').value) === "no")
     {
         document.querySelector('#refresh').value="yes";
@@ -31,17 +32,27 @@ document.addEventListener('DOMContentLoaded', () => {
     socket.on('connect', () => {
 
         
-        document.querySelector('form').onsubmit = () => {
+        document.querySelector('button').onclick  = () => {
                 const roomname = document.querySelector('input').value;
                 socket.emit('new room', {'roomname': roomname});
             };
         });
 
+        var inputText = document.getElementById("input_field");
+        inputText.addEventListener("keyup", function(event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.getElementById("b").click();
+            }
+        });
+
     socket.on('add room', data => {
         const li = document.createElement('li');
         var a=data["chatid"];
-        li.innerHTML = `<a href="/chatroom/${a}">${data["roomname"]}</a>`;
+        li.innerHTML = `<a href="/chatroom/${parseInt(a)}">${data["roomname"]}</a>`;
         document.querySelector('ul').append(li);
+        document.querySelector('input').value = "";
+        document.querySelector('button').disabled = true;
     });    
 });
 
