bd9f10ef35b9af6adabbac3a4bcde7432accfc4b
shanmukhat1999 <57249299+shanmukhat1999@users.noreply.github.com>
2021-01-21 10:51:43 +0530

    Update chatroom.js

--- static/chatroom.js
+++ static/chatroom.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Connect to websocket
+    // Cannot submit empty value for input field
     document.querySelector('button').disabled = true;
     document.querySelector('input').onkeyup = () => {
         if(document.querySelector('input').value.length>0)
@@ -12,6 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
             document.querySelector('button').disabled = true;
         }
     }
+
+    //Refreshing when pressed back/front buttons
     if ((document.querySelector('#refresh').value) === "no")
     {
         document.querySelector('#refresh').value="yes";
@@ -21,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('#refresh').value="no";
         window.location=window.location;
     }
+    // saving the chatid in localstorage to the room number
     if(!(localStorage.getItem("chatid")))
     {
         var k=document.querySelector('h2').innerHTML;
@@ -39,33 +42,40 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         localStorage.setItem("chatid",k);
     }
-
+    //connect to web socket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    // When connected, configure buttons
     
-    
+    // when leaving the room remove this chatid from local storage
     document.querySelector('#leave').onclick = () => {
         localStorage.removeItem("chatid");
     }
     
-
     socket.on('connect', () => {
 
-        document.querySelector('form').onsubmit =  () => {
-                const message = document.querySelector('input').value;
-                var ci=document.querySelector('h2').innerHTML;
-                ci=parseInt(ci);
-                var name=document.querySelector('input').dataset.val;
-                socket.emit('new message', {'message': message,'chat_id':ci,'username':name});
-                document.querySelector('input').value='';
-                return false;
-            };   
-        });
+        //when new message is entered
+        document.querySelector('button').onclick =  () => {
+            const message = document.querySelector('input').value;
+            var ci=document.querySelector('h2').innerHTML;
+            ci=parseInt(ci);
+            var name=document.querySelector('input').dataset.val;
+            socket.emit('new message', {'message': message,'chat_id':ci,'username':name});
+        };   
+    });
 
+    var inputText = document.getElementById("input_field");
+    inputText.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("b").click();
+        }
+    });
+
+    //when server emits the add message    
     socket.on('add message', data => {
         var s=data["chatid"];
         s=parseInt(s);
+        document.querySelector('input').value = "";
         if(s == localStorage.getItem("chatid"))
         {
             const li = document.createElement('li');
