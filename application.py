2f3d29f0e92aa8bc209522db65f488ba17330ccd
shanmukhat1999 <57249299+shanmukhat1999@users.noreply.github.com>
2021-02-03 09:52:45 +0530

    Update application.py

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app,engineio_logger=True)
+Session(app)
 
 # configuring socketio
 app.config["SECRET_KEY"] = 'thisisasecret'
